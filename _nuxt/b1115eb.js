a9e4ec596e1796eff20ec117ba0fd31acb58f256
Mohammad Sulthan <37440405+mohsulthana@users.noreply.github.com>
2021-09-19 16:32:14 +0700

    Updates

---
+++ _nuxt/b1115eb.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{247:function(t,e,n){"use strict";n.r(e);var o=n(44),component=Object(o.a)({},(function(){var t=this.$createElement;return(this._self._c||t)("footer",{staticClass:"footer-1 bg-gray-100 "})}),[],!1,null,null,null);e.default=component.exports;installComponents(component,{Footer:n(247).default})}}]);
